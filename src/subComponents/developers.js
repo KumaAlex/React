0b7d521c9103a225b30f76b30cd985a52d24c504
Aleksandr <male225@mail.ru>
2022-11-11 13:14:48 +0600

    developers

--- src/subComponents/developers.js
+++ src/subComponents/developers.js
@@ -8,21 +8,24 @@ export function Jopa() {
       Surname: "Kim",
       Age: 19,
       Gender: "male",
-      Status: "Junior WEB developer, 3rd year student of KBTU university",
+      Img: "https://www.pngkey.com/png/full/83-834045_download-panda-png.png",
+      Status: "subJunior WEB developer, 3rd year student of KBTU university",
     }, 
     {
       Name: "Azimbek",
       Surname: "Abdipattaev",
       Age: 19,
       Gender: "male",
-      Status: "Medium WEB developer, 3rd year student of KBTU university",
+      Img: "https://sun1-86.userapi.com/s/v1/ig2/QUyMnwyMSRsuy0kb2QjiOddttrlOMVSezLi9zLupG7tUywN7PL5I2-p_4UFg02XolwuyrYhHIyWXBEP9dXSSZrLV.jpg?size=400x400&quality=95&crop=0,111,469,469&ava=1",
+      Status: "Junior WEB developer, 3rd year student of KBTU university",
     },
     {
       Name: "Alikhan",
       Surname: "Kaliev",
       Age: 20,
       Gender: "male",
-      Status: "LOH, Junior WEB developer, 3rd year student of KBTU university",
+      Img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7nUw0zFMXd7_X0f72F9rP1z3E6mNkVSWyDQ&usqp=CAU",
+      Status: "Junior WEB developer, 3rd year student of KBTU university",
     }
   ]
   
@@ -34,7 +37,8 @@ export function Jopa() {
       demoJopa.push( (
         <tr>
               <th>
-                <img className='UserImg' src='https://pngimg.com/uploads/panda/panda_PNG8.png'></img>
+                
+                <img className='UserImg' src={i.Img}></img>
                 </th>
                 <th>
                   <ul>
