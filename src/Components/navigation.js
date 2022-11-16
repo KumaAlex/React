79cedc545f52ee0881c4ef31b3a270612fdca65f
Aleksandr <male225@mail.ru>
2022-11-16 10:08:33 +0600

    Turn navigation-bar on the left side

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -7,12 +7,12 @@ export function Navigation() {
   }
     return (
         <div className="nav-bar" id = "navPart">
-          <Link to={"/"}><button className="nav-component" onClick={windowLocation}>Main</button></Link>
-          <Link to={"/movies"}><button className="nav-component" onClick={windowLocation}>Movies</button></Link>
-          <Link to={"/premieres"}><button className="nav-component" onClick={windowLocation}>Premieres</button></Link>
-          <Link to={"/browse"}><button className="nav-component" onClick={windowLocation}>Browse</button></Link>
-          <Link to={"/about"}><button className="nav-component" onClick={windowLocation}>About Us</button></Link>
-          <Link to={"/profile"}><button className="nav-component" onClick={windowLocation}>Profile</button></Link>
+          <div><Link to={"/"}><button className="nav-component" onClick={windowLocation}>Main</button></Link></div>
+          <div><Link to={"/movies"}><button className="nav-component" onClick={windowLocation}>Movies</button></Link></div>
+          <div><Link to={"/premieres"}><button className="nav-component" onClick={windowLocation}>Premieres</button></Link></div>
+          <div><Link to={"/browse"}><button className="nav-component" onClick={windowLocation}>Browse</button></Link></div>
+          <div><Link to={"/about"}><button className="nav-component" onClick={windowLocation}>About Us</button></Link></div>
+          <div><Link to={"/profile"}><button className="nav-component" onClick={windowLocation}>Profile</button></Link></div>
         </div>
     )
 }
